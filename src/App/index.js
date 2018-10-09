c17802775174da1cc88809447a3b348d9b223c0c
Carol Enache <enache.carol@gmail.com>
2018-10-09 17:10:39 +0100

    sort the location id baset on the location where is droped

--- src/App/index.js
+++ src/App/index.js
@@ -12,13 +12,45 @@ const Title = styled.div`
 
 class App extends Component {
   state = data;
+
+  onDragEnd = result => {
+    console.log(this.state)
+    const { destination, source, draggableId} = result;
+    if(!destination) {
+      return;
+    }
+    if(destination.draggableId === source.draggableId === draggableId.index) {
+      return;
+    }
+    const column = this.state.columns[source.droppableId];
+    console.log(column, 'column')
+    const newHeroIds = Array.from(column.heroId);
+    console.log(newHeroIds, 'newHeroIds')
+    newHeroIds.splice(source.index, 1);
+    newHeroIds.splice(destination.index, 0, draggableId);
+    const newColumn = {
+      ...column,
+      heroId: newHeroIds,
+    }
+    console.log(newColumn, 'newColumn')
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+    console.log(newState, 'newState')
+    this.setState(newState);
+  }
+
   render() {
     return (
       <Fragment>
         <Title>  
           <text>Avengers Infinity War</text>
         </Title>
-        <DragDropContext>
+        <DragDropContext onDragEnd={this.onDragEnd}>
           {this.state.columnsort.map(columnId => {
             const column = this.state.columns[columnId];
             const heroes = column.heroId.map(heroId => this.state.heroes[heroId]);
