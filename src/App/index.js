30eea885314702ba3597e62ffb1cb1c9d37e64a3
Carol Enache <enache.carol@gmail.com>
2018-10-10 15:51:19 +0100

    make the draggble components transferable to other components

--- src/App/index.js
+++ src/App/index.js
@@ -10,6 +10,10 @@ const Title = styled.div`
   padding: 10px;
 `;
 
+const Container = styled.div`
+  display: flex;
+`;
+
 class App extends Component {
   state = data;
 
@@ -21,22 +25,53 @@ class App extends Component {
     if(destination.draggableId === source.draggableId === draggableId.index) {
       return;
     }
-    const column = this.state.columns[source.droppableId];
-    const newHeroIds = Array.from(column.heroId);
-    newHeroIds.splice(source.index, 1);
-    newHeroIds.splice(destination.index, 0, draggableId);
-    const newColumn = {
-      ...column,
-      heroId: newHeroIds,
+    const begin = this.state.columns[source.droppableId];
+    console.log(begin)
+    const end = this.state.columns[destination.droppableId];
+    console.log(end)
+
+    if(begin === end) {
+      const newHeroIds = Array.from(begin.heroId);
+      newHeroIds.splice(source.index, 1);
+      newHeroIds.splice(destination.index, 0, draggableId);
+      const newColumn = {
+        ...begin,
+        heroId: newHeroIds,
+      }
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+      this.setState(newState);      
+      return;
     }
+    const beginHeroIds = Array.from(begin.heroId);
+    beginHeroIds.splice(source.index, 1);
+    const newBegin = {
+      ...begin,
+      heroId: beginHeroIds
+    };
+
+    const endHeroIds = Array.from(end.heroId);
+    endHeroIds.splice(destination.index, 0, draggableId);
+    const newEnd = {
+      ...end,
+      heroId: endHeroIds
+    };
+
     const newState = {
       ...this.state,
       columns: {
         ...this.state.columns,
-        [newColumn.id]: newColumn,
-      },
-    };
+        [newBegin.id]: newBegin,
+        [newEnd.id]: newEnd
+      }
+    }
     this.setState(newState);
+
   }
 
   render() {
@@ -46,11 +81,14 @@ class App extends Component {
           <text>Avengers Infinity War</text>
         </Title>
         <DragDropContext onDragEnd={this.onDragEnd}>
+        <Container>
+
           {this.state.columnsort.map(columnId => {
             const column = this.state.columns[columnId];
             const heroes = column.heroId.map(heroId => this.state.heroes[heroId]);
             return <Column key={Column.id} column={column} heroes={heroes} />;
           })}
+        </Container>
         </DragDropContext>
       </Fragment>
     );
